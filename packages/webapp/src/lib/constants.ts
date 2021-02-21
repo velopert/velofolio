0c493e3e9dcff0548a0ffa895023344223c13664
velopert <public.velopert@gmail.com>
2021-02-22 00:05:23 +0900

    Add periodToMonths map

--- packages/webapp/src/lib/constants.ts
+++ packages/webapp/src/lib/constants.ts
@@ -1,1 +1,12 @@
-export const periodOptions = ['Monthly', 'Quaterly', 'Semi-anually', 'Anually']
+export const periodOptions: string[] = [
+  'Monthly',
+  'Quaterly',
+  'Semi-anually',
+  'Anually',
+]
+export const periodToMonthsMap: Record<string, number> = {
+  Monthly: 1,
+  Quaterly: 3,
+  'Semi-anually': 6,
+  Anually: 12,
+}
