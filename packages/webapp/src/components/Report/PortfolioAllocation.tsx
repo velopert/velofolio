1c43505ac9c16943d0699f21d87b4bf30be174ab
velopert <public.velopert@gmail.com>
2021-03-20 00:22:07 +0900

    Implement sector weightings

--- packages/webapp/src/components/Report/PortfolioAllocation.tsx
+++ packages/webapp/src/components/Report/PortfolioAllocation.tsx
@@ -25,6 +25,7 @@ function PortfolioAllocation({ portfolio }: PortfolioAllocationProps) {
   const assetDetails = useAssetDetailsValue()
 
   const divRef = useRef<HTMLDivElement>(null)
+  const sectorChartDivRef = useRef<HTMLDivElement>(null)
 
   const seriesData = useMemo(
     () =>
@@ -35,7 +36,7 @@ function PortfolioAllocation({ portfolio }: PortfolioAllocationProps) {
     [portfolio, totalWeight]
   )
 
-  const chartOptions = useMemo(() => {
+  const pieChartOptions = useMemo(() => {
     if (!seriesData) return null
     const options: ECBasicOption = {
       color: chartColors,
@@ -64,7 +65,7 @@ function PortfolioAllocation({ portfolio }: PortfolioAllocationProps) {
           name: 'Asset Allocation',
           type: 'pie',
           radius: '75%',
-          center: ['50%', '50%'],
+          center: ['50%', '40%'],
           data: seriesData,
           label: {
             show: false,
@@ -76,52 +77,195 @@ function PortfolioAllocation({ portfolio }: PortfolioAllocationProps) {
     return options
   }, [seriesData])
 
-  useEffect(() => {
-    const element = divRef.current
-    if (!element || !chartOptions) return
+  const sectorWeightingsEntries = useMemo(() => {
+    if (!assetDetailsReady) return null
+    const sectorWeightingsRecord: Record<string, number> = {}
 
-    const chart = echarts.init(element)
-    chart.setOption(chartOptions)
+    portfolio.assets.forEach((asset) => {
+      const assetDetail = assetDetails[asset.ticker]?.data
+      if (!assetDetail) return
+
+      if (assetDetail.sector_weightings.length > 0) {
+        assetDetail.sector_weightings.forEach((weighting) => {
+          const currentValue = sectorWeightingsRecord[weighting.sector] ?? 0
+          const calculatedWeight = asset.weight * (weighting.percentage / 100)
+          sectorWeightingsRecord[weighting.sector] =
+            currentValue + calculatedWeight
+        })
+        return
+      }
+
+      const sector = assetDetail.sector || 'N/A'
+
+      const currentValue = sectorWeightingsRecord[sector] ?? 0
+      sectorWeightingsRecord[sector] = currentValue + asset.weight
+      return
+    })
+
+    return Object.entries(sectorWeightingsRecord)
+  }, [assetDetailsReady, assetDetails, portfolio.assets])
+
+  const barChartOptions = useMemo(() => {
+    if (!sectorWeightingsEntries) return null
+    const weightingSum = sectorWeightingsEntries.reduce(
+      (acc, current) => acc + current[1],
+      0
+    )
+    const categoryData = sectorWeightingsEntries.map(([sector]) => sector)
+    const seriesData = [
+      {
+        name: 'Sector Weightings',
+        type: 'bar',
+        data: sectorWeightingsEntries.map(
+          ([, weighting]) => weighting / weightingSum
+        ),
+      },
+    ]
+
+    const leftMargin =
+      Math.max(...categoryData.map((category) => category.length)) * 6 + 24
+
+    const options: ECBasicOption = {
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: { type: 'shadow' },
+        formatter: (params: any) => {
+          const data = params
+            .map(
+              (param: any) => `
+          <div style="display: flex; justify-content: space-between; align-items: center;">
+              <div style="display: flex; align-items: center;">
+                  <div style="border-radius: 6px; width: 12px; height: 12px; margin-right: 8px; background-color: ${
+                    param.color
+                  }"></div>
+                  <b style="margin-right: 48px;">${param.seriesName}</b>
+              </div>
+              ${convertToPercentage(param.data)}
+          </div>
+      `
+            )
+            .join('')
+          return `<div>${params[0].axisValueLabel}</div>`.concat(data)
+        },
+      },
+      yAxis: {
+        type: 'category',
+        data: categoryData,
+      },
+      xAxis: {
+        type: 'value',
+        axisLabel: {
+          formatter: (value: number) => {
+            return convertToPercentage(value, 2)
+          },
+        },
+      },
+      series: seriesData,
+      grid: {
+        left: leftMargin,
+        top: 0,
+        right: 32,
+        bottom: 32,
+      },
+    }
+
+    return options
+  }, [sectorWeightingsEntries])
+
+  useEffect(() => {
+    const pieElement = divRef.current
+    if (!pieElement || !pieChartOptions) return
+
+    const pieChart = echarts.init(pieElement)
+    pieChart.setOption(pieChartOptions)
 
     const handleResize = () => {
-      chart.resize()
+      pieChart.resize()
     }
     window.addEventListener('resize', handleResize)
     return () => {
       window.removeEventListener('resize', handleResize)
     }
-  }, [chartOptions])
+  }, [pieChartOptions])
 
-  if (!assetDetailsReady) return null
+  useEffect(() => {
+    const sectorChartElement = sectorChartDivRef.current
+    if (!sectorChartElement || !barChartOptions) return
+
+    const fnWrapper = { handleResize: () => {} }
+
+    setTimeout(() => {
+      const barChart = echarts.init(sectorChartElement)
+      barChart.setOption(barChartOptions)
+
+      fnWrapper.handleResize = () => {
+        barChart.resize()
+      }
+    }, 100)
+
+    const fn = () => fnWrapper.handleResize()
+
+    window.addEventListener('resize', () => fn)
+    return () => {
+      window.removeEventListener('resize', fn)
+    }
+  }, [barChartOptions])
+
+  if (!assetDetailsReady || !sectorWeightingsEntries) return null
 
   return (
-    <div css={block}>
-      <div css={pane}>
-        <table css={tableStyle}>
-          <thead>
-            <tr>
-              <th>Ticker</th>
-              <th>Stock Name</th>
-              <th>%</th>
-            </tr>
-          </thead>
-          <tbody>
-            {portfolio.assets.map((asset) => (
-              <tr key={asset.id}>
-                <td>{asset.ticker}</td>
-                <td>{assetDetails[asset.ticker]?.data?.name ?? ''}</td>
-                <td>{convertToPercentage(asset.weight / totalWeight)}</td>
+    <div css={wrapper}>
+      <h4>{portfolio.name}</h4>
+      <h5>Sector Weightings</h5>
+      <div
+        css={sectorChartStyle(sectorWeightingsEntries.length)}
+        ref={sectorChartDivRef}
+      ></div>
+
+      <h5>Asset Weightings</h5>
+      <div css={block}>
+        <div css={pane}>
+          <table css={tableStyle}>
+            <thead>
+              <tr>
+                <th>Ticker</th>
+                <th>Stock Name</th>
+                <th>%</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      <div css={pane}>
-        <div css={chartStyle} ref={divRef}></div>
+            </thead>
+            <tbody>
+              {portfolio.assets.map((asset) => (
+                <tr key={asset.id}>
+                  <td>{asset.ticker}</td>
+                  <td>{assetDetails[asset.ticker]?.data?.name ?? ''}</td>
+                  <td>{convertToPercentage(asset.weight / totalWeight)}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div css={pane}>
+          <div css={pieChartStyle} ref={divRef}></div>
+        </div>
       </div>
     </div>
   )
 }
+
+const wrapper = css`
+  padding-top: 0.5rem;
+  h4,
+  h5 {
+    color: ${palette.blueGrey[800]};
+  }
+
+  h4 {
+    margin-bottom: 0;
+  }
+  h5 {
+    margin-top: 1rem;
+  }
+`
 
 const block = css`
   display: flex;
@@ -162,9 +306,14 @@ const tableStyle = css`
   }
 `
 
-const chartStyle = css`
+const pieChartStyle = css`
   width: 100%;
   height: 20rem;
+`
+
+const sectorChartStyle = (sectorCount: number) => css`
+  width: 100%;
+  height: ${Math.max(sectorCount * 2.5, 15)}rem;
 `
 
 export default PortfolioAllocation
