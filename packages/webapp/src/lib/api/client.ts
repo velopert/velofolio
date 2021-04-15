37160f2003ba071cf03a297d77c782a20cc9e49d
velopert <public.velopert@gmail.com>
2021-04-15 23:19:13 +0900

    Change production api path

--- packages/webapp/src/lib/api/client.ts
+++ packages/webapp/src/lib/api/client.ts
@@ -3,6 +3,6 @@ import axios from 'axios'
 const client = axios.create()
 
 client.defaults.baseURL =
-  process.env.NODE_ENV === 'development' ? '' : 'https://api.velofol.io'
+  process.env.NODE_ENV === 'development' ? '' : 'https://velofolio.vlpt.us'
 
 export default client
