03557e05c45b20385318c0b76016e7467b5cd25b
velopert <public.velopert@gmail.com>
2021-04-15 23:23:57 +0900

    Change temp api domain

--- packages/webapp/src/lib/api/client.ts
+++ packages/webapp/src/lib/api/client.ts
@@ -3,6 +3,6 @@ import axios from 'axios'
 const client = axios.create()
 
 client.defaults.baseURL =
-  process.env.NODE_ENV === 'development' ? '' : 'https://velofolio.vlpt.us'
+  process.env.NODE_ENV === 'development' ? '' : 'https://velofolio-api.vlpt.us'
 
 export default client
