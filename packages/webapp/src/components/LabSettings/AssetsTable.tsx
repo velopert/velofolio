c3aacce93b8049645dbd7a95ba4812bcf2a9de56
velopert <public.velopert@gmail.com>
2021-04-24 07:06:53 +0900

    Resolves issue #11

--- packages/webapp/src/components/LabSettings/AssetsTable.tsx
+++ packages/webapp/src/components/LabSettings/AssetsTable.tsx
@@ -71,8 +71,9 @@ function AssetsTable({ focusInput }: AssetsTableProps) {
               <input
                 value={asset.weight}
                 type="number"
+                step=".01"
                 onChange={(e) => {
-                  const value = parseInt(e.target.value)
+                  const value = parseFloat(e.target.value)
                   updateWeight(asset.id, isNaN(value) ? 0 : value)
                 }}
               />
