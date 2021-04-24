cc4b9b1f084d89253a189c82af750ec9744ba7ed
velopert <public.velopert@gmail.com>
2021-04-24 13:29:36 +0900

    Responsive layout for indicators

--- packages/webapp/src/components/Report/IndicatorsSection.tsx
+++ packages/webapp/src/components/Report/IndicatorsSection.tsx
@@ -3,6 +3,7 @@ import { useEffect, useMemo } from 'react'
 import { useInitialAmountState } from '../../atoms/labSettingState'
 import { useMonthsCount, useReportValue } from '../../atoms/reportState'
 import palette from '../../lib/palette'
+import media from '../../lib/styles/media'
 import {
   convertToPercentage,
   returnRate,
@@ -23,6 +24,37 @@ function IndicatorsSection({}: IndicatorsSectionProps) {
     monthsCount,
   ])
 
+  const indicators = useMemo(() => {
+    return report.map(
+      ({
+        id,
+        indicator: {
+          finalBalance,
+          cagr,
+          stdev,
+          mdd,
+          sharpeRatio,
+          sortinoRatio,
+          best,
+          worst,
+        },
+        name,
+      }) => ({
+        id,
+        name,
+        finalBalance: `$${Math.floor(finalBalance).toLocaleString()} (
+          ${convertToPercentage(returnRate(initialAmount, finalBalance))})`,
+        cagr: convertToPercentage(cagr),
+        stdev: convertToPercentage(stdev),
+        mdd: convertToPercentage(mdd),
+        best: convertToPercentage(moreThanTwoYears ? best.year : best.month),
+        worst: convertToPercentage(moreThanTwoYears ? worst.year : worst.month),
+        sharpe: sharpeRatio.toFixed(2),
+        sortino: sortinoRatio ? sortinoRatio.toFixed(2) : 'N/A',
+      })
+    )
+  }, [initialAmount, moreThanTwoYears, report])
+
   if (report.length === 0) return null
 
   return (
@@ -42,53 +74,103 @@ function IndicatorsSection({}: IndicatorsSectionProps) {
           </tr>
         </thead>
         <tbody>
-          {report.map(
+          {indicators.map(
             ({
               id,
-              indicator: {
-                finalBalance,
-                cagr,
-                stdev,
-                mdd,
-                sharpeRatio,
-                sortinoRatio,
-                best,
-                worst,
-              },
               name,
+              best,
+              cagr,
+              finalBalance,
+              mdd,
+              sharpe,
+              sortino,
+              stdev,
+              worst,
             }) => (
               <tr key={id}>
                 <td>{name}</td>
-                <td>
-                  ${Math.floor(finalBalance).toLocaleString()} (
-                  {convertToPercentage(returnRate(initialAmount, finalBalance))}
-                  )
-                </td>
-                {moreThanOneYear && <td>{convertToPercentage(cagr)}</td>}
-                <td>{convertToPercentage(stdev)}</td>
-                <td>{convertToPercentage(mdd)}</td>
-                <td>
-                  {convertToPercentage(
-                    moreThanTwoYears ? best.year : best.month
-                  )}
-                </td>
-                <td>
-                  {convertToPercentage(
-                    moreThanTwoYears ? worst.year : worst.month
-                  )}
-                </td>
-                <td>{sharpeRatio.toFixed(2)}</td>
-                <td>{sortinoRatio ? sortinoRatio.toFixed(2) : 'N/A'}</td>
+                <td>{finalBalance}</td>
+                {moreThanOneYear && <td>{cagr}</td>}
+                <td>{stdev}</td>
+                <td>{mdd}</td>
+                <td>{best}</td>
+                <td>{worst}</td>
+                <td>{sharpe}</td>
+                <td>{sortino}</td>
               </tr>
             )
           )}
         </tbody>
       </table>
+      <div css={mobileView}>
+        {indicators.map(
+          ({
+            id,
+            name,
+            best,
+            cagr,
+            finalBalance,
+            mdd,
+            sharpe,
+            sortino,
+            stdev,
+            worst,
+          }) => (
+            <div key={id} className="item">
+              <div className="row">
+                <div className="name">Portfolio Name</div>
+                <div className="value">{name}</div>
+              </div>
+              <div className="row">
+                <div className="name">Final Balance</div>
+                <div className="value">{finalBalance}</div>
+              </div>
+              {moreThanOneYear && (
+                <div className="row">
+                  <div className="name">CAGR</div>
+                  <div className="value">{cagr}</div>
+                </div>
+              )}
+              <div className="row">
+                <div className="name">Stdev</div>
+                <div className="value">{stdev}</div>
+              </div>
+              <div className="row">
+                <div className="name">Max Drawdown</div>
+                <div className="value">{mdd}</div>
+              </div>
+              <div className="row">
+                <div className="name">
+                  Best {moreThanTwoYears ? 'Year' : 'Month'}
+                </div>
+                <div className="value">{best}</div>
+              </div>
+              <div className="row">
+                <div className="name">
+                  Worst {moreThanTwoYears ? 'Year' : 'Month'}
+                </div>
+                <div className="value">{worst}</div>
+              </div>
+              <div className="row">
+                <div className="name">Sharpe Ratio</div>
+                <div className="value">{sharpe}</div>
+              </div>
+              <div className="row">
+                <div className="name">Sortino Ratio</div>
+                <div className="value">{sortino}</div>
+              </div>
+            </div>
+          )
+        )}
+      </div>
     </ReportSection>
   )
 }
 
 const tableStyle = css`
+  ${media.xlarge} {
+    display: none;
+  }
   width: 100%;
   border-collapse: collapse;
   font-size: 0.875rem;
@@ -108,6 +190,33 @@ const tableStyle = css`
 
   thead th {
     color: ${palette.blueGrey[800]};
+  }
+`
+
+const mobileView = css`
+  font-size: 0.875rem;
+  display: none;
+  ${media.xlarge} {
+    display: block;
+  }
+
+  .item {
+    .row {
+      .name {
+        color: ${palette.blueGrey[800]};
+        font-weight: bold;
+      }
+      .value {
+        color: ${palette.blueGrey[600]};
+        margin-top: 0.5rem;
+      }
+    }
+    .row + .row {
+      margin-top: 1rem;
+    }
+  }
+  .item + .item {
+    margin-top: 2rem;
   }
 `
 
