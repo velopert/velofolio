c7b60f4fc0e06f26e4e84795e7e6d883530ada79
velopert <public.velopert@gmail.com>
2021-04-27 23:38:46 +0900

    Fix typo
    아 쪽팔려 ㅋㅋㅋ

--- packages/webapp/src/lib/constants.ts
+++ packages/webapp/src/lib/constants.ts
@@ -1,13 +1,13 @@
 export const periodOptions: string[] = [
   'Monthly',
-  'Quaterly',
-  'Semi-anually',
-  'Anually',
+  'Quarterly',
+  'Semi-annually',
+  'Annually',
 ]
 export const periodToMonthsMap: Record<string, number> = {
   Monthly: 1,
-  Quaterly: 3,
-  'Semi-anually': 6,
+  Quarterly: 3,
+  'Semi-annually': 6,
   Anually: 12,
 }
 
